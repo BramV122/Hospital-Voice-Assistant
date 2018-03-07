01187e50b6db26657b4100bd13ec7f9f1cffe414
Bram Verstraeten <bramverstraeten@NOREPLY.com>
2018-03-07 21:53:39 +0100

    experiment V3

--- text_assistant.py
+++ text_assistant.py
@@ -1,10 +1,17 @@
-import googlesamples.assistant.grpc.textinput
+import textinput
 import google.auth.transport.grpc
 import google.auth.transport.requests
 import google.oauth2.credentials
 import aiy.assistant.device_helpers as device_helpers
+import googlesamples.assistant.grpc.audio_helpers as audio_helpers
 
-class Text_Assistant(googlesamples.assistant.grpc.textinput.SampleTextAssistant):
+audio_sample_rate = audio_helpers.DEFAULT_AUDIO_SAMPLE_RATE
+audio_sample_width = audio_helpers.DEFAULT_AUDIO_SAMPLE_WIDTH
+audio_block_size = audio_helpers.DEFAULT_AUDIO_DEVICE_BLOCK_SIZE
+audio_flush_size = audio_helpers.DEFAULT_AUDIO_DEVICE_FLUSH_SIZE
+audio_iter_size = audio_helpers.DEFAULT_AUDIO_ITER_SIZE
+
+class Text_Assistant(textinput.SampleTextAssistant):
 
     def __init__(self, credentials):
         self._credentials = credentials
@@ -18,4 +25,30 @@ class Text_Assistant(googlesamples.assistant.grpc.textinput.SampleTextAssistant)
 
         grpc_channel = google.auth.transport.grpc.secure_authorized_channel(credentials, http_request, api_endpoint)
 
-        super().__init__('en-US', self._model_id, self._device_id, grpc_channel, grpc_deadline)
+        audio_device = None
+        audio_source = audio_device = (
+            audio_device or audio_helpers.SoundDeviceStream(
+                sample_rate=audio_sample_rate,
+                sample_width=audio_sample_width,
+                block_size=audio_block_size,
+                flush_size=audio_flush_size
+            )
+        )
+        audio_sink = audio_device = (
+            audio_device or audio_helpers.SoundDeviceStream(
+                sample_rate=audio_sample_rate,
+                sample_width=audio_sample_width,
+                block_size=audio_block_size,
+                flush_size=audio_flush_size
+            )
+        )
+
+        # Create conversation stream with the given audio source and sink.
+        conversation_stream = audio_helpers.ConversationStream(
+            source=audio_source,
+            sink=audio_sink,
+            iter_size=audio_iter_size,
+            sample_width=audio_sample_width,
+        )
+
+        super().__init__('en-US', self._model_id, self._device_id, grpc_channel, grpc_deadline, conversation_stream)
---
+++ textinput.py
@@ -0,0 +1,102 @@
+# Copyright (C) 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Sample that implements a text client for the Google Assistant Service."""
+
+from google.assistant.embedded.v1alpha2 import (
+    embedded_assistant_pb2,
+    embedded_assistant_pb2_grpc
+)
+
+try:
+    from . import (
+        assistant_helpers,
+    )
+except (SystemError, ImportError):
+    import assistant_helpers
+
+
+ASSISTANT_API_ENDPOINT = 'embeddedassistant.googleapis.com'
+DEFAULT_GRPC_DEADLINE = 60 * 3 + 5
+
+
+class SampleTextAssistant(object):
+    """Sample Assistant that supports text based conversations.
+    Args:
+      language_code: language for the conversation.
+      device_model_id: identifier of the device model.
+      device_id: identifier of the registered device instance.
+      channel: authorized gRPC channel for connection to the
+        Google Assistant API.
+      deadline_sec: gRPC deadline in seconds for Google Assistant API call.
+    """
+
+    def __init__(self, language_code, device_model_id, device_id,
+                 channel, deadline_sec, conversation_stream):
+        self.language_code = language_code
+        self.device_model_id = device_model_id
+        self.device_id = device_id
+        self.conversation_state = None
+        self.assistant = embedded_assistant_pb2_grpc.EmbeddedAssistantStub(
+            channel
+        )
+        self.deadline = deadline_sec
+        self.conversation_stream = conversation_stream
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, etype, e, traceback):
+        if e:
+            return False
+
+    def assist(self, text_query):
+        """Send a text request to the Assistant and playback the response.
+        """
+        def iter_assist_requests():
+            dialog_state_in = embedded_assistant_pb2.DialogStateIn(
+                language_code=self.language_code,
+                conversation_state=b''
+            )
+            if self.conversation_state:
+                dialog_state_in.conversation_state = self.conversation_state
+            config = embedded_assistant_pb2.AssistConfig(
+                audio_out_config=embedded_assistant_pb2.AudioOutConfig(
+                    encoding='LINEAR16',
+                    sample_rate_hertz=16000,
+                    volume_percentage=80,
+                ),
+                dialog_state_in=dialog_state_in,
+                device_config=embedded_assistant_pb2.DeviceConfig(
+                    device_id=self.device_id,
+                    device_model_id=self.device_model_id,
+                ),
+                text_query=text_query,
+            )
+            req = embedded_assistant_pb2.AssistRequest(config=config)
+            assistant_helpers.log_assist_request_without_audio(req)
+            yield req
+
+        display_text = None
+        for resp in self.assistant.Assist(iter_assist_requests(),
+                                          self.deadline):
+            assistant_helpers.log_assist_response_without_audio(resp)
+            if resp.dialog_state_out.conversation_state:
+                conversation_state = resp.dialog_state_out.conversation_state
+                self.conversation_state = conversation_state
+                if len(resp.audio_out.audio_data) > 0:
+                    self.conversation_stream.write(resp.audio_out.audio_data)
+            if resp.dialog_state_out.supplemental_display_text:
+                display_text = resp.dialog_state_out.supplemental_display_text
+        return display_text
